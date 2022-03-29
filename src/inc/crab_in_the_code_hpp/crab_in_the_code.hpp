6101c16d739f143f48b5886570d6b23abeaa0706
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-03-29 02:59:20 -0700

    ⚗ uh

---
+++ src/inc/crab_in_the_code_hpp/crab_in_the_code.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Aubrey on 3/28/2022.
+//
+
+#ifndef CRAB_IN_THE_CODE_HPP_CRAB_IN_THE_CODE_HPP
+#define CRAB_IN_THE_CODE_HPP_CRAB_IN_THE_CODE_HPP
+
+#include <iostream>
+
+namespace crab_in_the_code {
+    const static std::string CRAB_IN_THE_CODE_FUCK_YOU = "🦀 CRAB IN THE COOOOOOOOOOOOOOOOOOOOOOOOODE 🦀";
+    const static char* CRAB_IN_THE_CODE_FUCK_YOU = "🦀 CRAB IN THE COOOOOOOOOOOOOOOOOOOOOOOOODE 🦀";
+}
+
+#endif //CRAB_IN_THE_CODE_HPP_CRAB_IN_THE_CODE_HPP
